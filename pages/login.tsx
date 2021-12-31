dda33c91cad7b956aab8156e39080f5f8759bbcc
sammydowds <sammydowds1993@gmail.com>
2021-12-31 15:26:54 -0600

    fix: del prop

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,12 +1,12 @@
 
 /**@jsxImportSource @emotion/react */
 import { LoginForm } from "../components/LoginForm"
-import { Heading, Flex } from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react'
 
 const Login = () => {
     return(
       <Flex direction='column' justify='space-around' align='center' minH='100vh' spacing={10}>
-	<LoginForm loading/>
+	<LoginForm />
       </Flex>
     )
 }
